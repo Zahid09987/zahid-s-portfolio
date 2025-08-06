3a80dfaea1e1bfa96232950c773c4fd28b0c4850
Zahid09987 <zahidrizkyfakhri@gmail.com>
2025-08-06 19:02:56 +0700

    added 404 page

---
+++ pages/404.php
@@ -0,0 +1,8 @@
+<div class="container-fluid py-5 text-center">
+    <h1 class="display-1 fw-bold">404</h1>
+    <p class="fs-3"> <span class="text-danger">Oops!</span> Page not found.</p>
+    <p class="lead">
+        The page you're looking for doesn't exist.
+    </p>
+    <a href="index.php" class="btn btn-primary">Go Home</a>
+</div>
