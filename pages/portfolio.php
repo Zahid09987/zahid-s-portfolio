56cdb983175ea465fa7f4be8350d254137710986
Zahid09987 <zahidrizkyfakhri@gmail.com>
2025-08-06 18:53:56 +0700

    added portfolio and an image

---
+++ pages/portfolio.php
@@ -0,0 +1,14 @@
+<div class="container-fluid py-5">
+    <h1 class="page-title">Zahid's Work</h1>
+    <div class="row">
+        <div class="col-md-6 col-lg-4 mb-4">
+            <div class="card h-100 glass-effect">
+                <img src="images/UI-UX-Project.jpg" class="card-img-top" alt="UI/UX Project">
+                <div class="card-body">
+                    <h5 class="card-title">UI/UX Project</h5>
+                    <p class="card-text">This is the Project of an e-parking inside and outside UPJ we want to make, but has abandoned.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
