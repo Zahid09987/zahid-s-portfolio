3a6430c39b8ed02465d03fafd8d5810b298e3872
Zahid09987 <zahidrizkyfakhri@gmail.com>
2025-08-06 17:57:02 +0700

    added about and home page

---
+++ pages/home.php
@@ -0,0 +1,5 @@
+<div class="container-fluid py-5 text-center">
+    <h1 class="display-4 fw-bold page-title">Welcome to Zahid's Portfolio</h1>
+    <p class="fs-4 mb-4">He loves what he does.</p>
+    <a href="?page=portfolio" class="btn btn-primary btn-lg">View His Work</a>
+</div>
